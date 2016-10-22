ea8e00ebb75d584c021e857ada25e086f8b5cc97
Hrishikesh Suresh <sureshh@mcmaster.ca>
2016-10-22 13:15:33 -0400

    show response

--- js/main.js
+++ js/main.js
@@ -10,7 +10,19 @@ var time = 15;
 var fingerDown = false;
 var lastTap = 0; 
 
+var LRTapCount = 0;
+var LRIntervals = [];
+var rightZone = false; //To detect which region we are in
+var rightExited = false; //Once the tap is completed, we don't wanna throw error until they leave the region'
+var rightTapped = false; //To prevent taps from being done in the same region more than once
+var leftZone = false;
+var leftExited = false;
+var leftTapped = false;
+var expectedDirection = 1; //1 is right and -1 is right, it will be inverted with every tap
+
+
 function countSingleTaps() {
+    startTime = null; 
     Leap.loop(controllerOptions, function(frame) {
 
 
@@ -23,7 +35,7 @@ function countSingleTaps() {
             messages = "No hands are visible. Please make sure your right hands is over the sensor";
         //Make sure that both hands are not in view
         } else if (frame.hands.length == 2) {
-            messages = "You have places both your hands over the sensor, please remove your left hands"; 
+            messages = "You have placed both your hands over the sensor, please remove your left hands"; 
         } else {
             var hand = frame.hands[0];
             //Ensure that patient is using their right hand
@@ -51,7 +63,7 @@ function countSingleTaps() {
                         data =  {
                             "Inputs": {
                                 "input1": { 
-                                    "ColumnNames": ["count", "avg", "variant", "has?"],
+                                    "ColumnNames": ["taps", "int", "stdev", "has?"],
                                     "Values": [singleTapCount, avg, stdev, "FALSE"]
                                 },        
                             },
@@ -91,7 +103,6 @@ function countSingleTaps() {
                     //Get the instance of the hand then the index finger
                     var indexFinger = hand.indexFinger;
                     var fingerPosition = indexFinger.tipPosition;
-                    var stabilizedPosition = indexFinger.stabilizedTipPosition;
 
                     //When the finger goes down, mark it as down and then when it comes back up, increase the tap count
                     //and then note the interval to the last tap if there has been more than one tap
@@ -125,4 +136,70 @@ function updateUI() {
     document.querySelector('.messages').innerHTML = messages;
     document.querySelector('.data').innerHTML = dataArray;
     document.querySelector('.time').innerHTML = time;
+}
+
+function countLeftRightTaps() {
+    startTime = null; 
+    Leap.loop(controllerOptions, function(frame) {
+
+
+        if ( startTime != null && time > 0) {
+            time = 15 - Math.floor((frame.timestamp - startTime)/1000000);
+        }
+
+        //Make sure that hands are visible before the timer starts
+        if (frame.hands.length == 0) {
+            messages = "No hands are visible. Please make sure your right hands is over the sensor";
+        //Make sure that both hands are not in view
+        } else if (frame.hands.length == 2) {
+            messages = "You have placed both your hands over the sensor, please remove your left hands"; 
+        } else {
+            var hand = frame.hands[0];
+            //Ensure that patient is using their right hand
+            if (hand.type == "right") { 
+                 if (hand.pitch() < -0.10 || hand.pitch > 0.15) {
+                    messages = "Your hand is not flat. Please make sure your hand it parallel to the floor"
+                } else {
+                    //If the start time is not set, start it
+                    if (startTime == null) {
+                        startTime = frame.timestamp;
+                    }
+                    // Once 15 seconds have passed, return the counts and the intervals
+                    if (frame.timestamp - startTime >= 15000000) {
+                        //Code to send the data to the server
+                    }
+
+                    //Get the instance of the hand then the index finger
+                    var indexFinger = hand.indexFinger;
+                    var fingerPosition = indexFinger.tipPosition;
+
+                    if (fingerPosition[0] > 45) {
+                        rightZone = true;
+                    } else if (fingerPosition[0] < -45) {
+                        leftZone = true;
+                    } else {
+                        rightZone = false;
+                        leftZone = false;
+                    }
+
+                    if (expectedDirection == 1 && rightZone && rightExited == false ) {
+                        if (fingerPosition[1]  < 100) {
+                            fingerDown = true;
+                        } else if (fingerPosition[1]  > 100 && fingerDown == true) {
+                            LRTapCount += 1;
+                            fingerDown = false;
+                            expectedDirection *= -1; 
+                            if (LRTapCount > 1) {
+                                var intervalC = frame.timestamp - lastTap;
+                                intervals.push(intervalC);
+                                lastTap = frame.timestamp;
+                            } else {
+                                lastTap = frame.timestamp;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
 }
